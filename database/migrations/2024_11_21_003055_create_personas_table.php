ce5b720edee612c31bad9c77321b2a8c9459118f
Manuel Eduardo Martinez Ramirez <lalofrancia1@gmail.com>
2024-11-21 20:27:24 -0600

    Tablas, relaciones y modelos

---
+++ database/migrations/2024_11_21_003055_create_personas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('razon_social', 80);
+            $table->string('direccion', 80);
+            $table->string('tipo_persona', 20);
+            $table->foreignId('documento_id')->unique()->constrained('documentos')->onDelete('cascade');
+            $table->tinyInteger('estado')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ resources/views/template.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Sistema de Ventas" />
+        <meta name="author" content="Equipo de Eduard y Manuel" />
+        <title>Sistema de Ventas - @yield('title')</title>
+        <!---<link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />-->
+        <link href="{{ asset('css/template.css') }}" rel="stylesheet"/>
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        @stack('css')
+    </head>
+    <body class="sb-nav-fixed">
+    <x-navigation-header></x-navigation-header>
+        
+        <div id="layoutSidenav">
+            <x-navigation-menu></x-navigation-menu>
+            <div id="layoutSidenav_content">
+                <main>
+                    @yield('content')
+                </main>
+                <x-footer></x-footer>
+                
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{asset('js/scripts.js')}}"></script>
+        @stack('js')
+
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    </body>
+</html>
