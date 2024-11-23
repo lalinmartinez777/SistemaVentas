a137846d91c6990709792f3b2b954d2e0d683988
Eduard Delgado <eduard.delgado2861@alumnos.udg.mx>
2024-11-22 23:26:18 -0600

    Creación de controladoŕes de usuarios y coprobates, así compo creación de las vistas correspondientes

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
     use HasFactory;
 
     public function persona(){
         return $this->belongsTo(Persona::class);
     }
 
+    public function ventas(){
+        return $this->hasMany(Venta::class);
+    }
+
+    protected $fillable = ['persona_id'];
 }
