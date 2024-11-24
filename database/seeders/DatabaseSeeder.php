6535b160c6404c5c06b365d50013ec05f0073f32
Manuel Eduardo Martinez Ramirez <lalofrancia1@gmail.com>
2024-11-23 19:05:04 -0600

    correccion de nombre en seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
-class UserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
